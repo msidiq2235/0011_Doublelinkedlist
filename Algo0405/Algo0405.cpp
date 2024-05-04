df53c667e3b9f50c4e0514bac7be11b708deb360
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-05 06:04:30 +0700

    4

--- Algo0405/Algo0405.cpp
+++ Algo0405/Algo0405.cpp
@@ -36,4 +36,16 @@ void addNode()
 		newNode->prev = NULL; //step 5:make the new node point to NULL
 		START = newNode; // step 6: make the new node the first node
 	}
+	else
+	{
+		//insert the new node in the middle or at the end
+		Node* current = START;// step 1.a: start from the first node
+		Node* previous = NULL;// step1.b: previouus node is NULL initially
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{							//step 1.c: traverse the list to find the correct position
+			previous = current;		//step 1.d: move the previous to the current node
+			current = current->next;//step 1.e: move the current to the next node
+		}
+	}
 }
