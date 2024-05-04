5b3d8ee15e824c8047ee1a440d3a56b3574c6349
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-05 06:10:37 +0700

    17

--- Algo0405/Algo0405.cpp
+++ Algo0405/Algo0405.cpp
@@ -191,3 +191,38 @@ void searchData()
 }
 
 int main()
+{
+	while (true)
+	{
+		try
+		{
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record to the list" << endl;
+			cout << "3. View all records in the ascending order of roll numbers" << endl;
+			cout << "4. View all record in the descending order of roll numbers " << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): ";
+			char ch;
+			cin >> ch;
+
+			switch (ch)
+			{
+			case '1':
+				addNode();
+				break;
+			case '2':
+				deleteNode();
+				break;
+			case '3':
+				traverse();
+				break;
+			case '4':
+				revtraverse();
+				break;
+
+			}
+		}
+	}
+}
