a3631c371309306661cc427320f9348b61c91d51
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-05 06:03:50 +0700

    3

--- Algo0405/Algo0405.cpp
+++ Algo0405/Algo0405.cpp
@@ -11,3 +11,29 @@ struct Node
 
 Node* START = NULL;
 
+void addNode()
+{
+	Node* newNode = new Node(); //step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; //assingn values to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; //assign value to the data field of the new node
+
+	//insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{//step 2: insert the new node at the beginning
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+		// if the list is empty, make the new node the START
+		newNode->next = START; //step 3: make the new node point to the first node
+		if (START != NULL)
+		{
+			START->prev = newNode; //step 4: make the first node point to the new node
+		}
+		newNode->prev = NULL; //step 5:make the new node point to NULL
+		START = newNode; // step 6: make the new node the first node
+	}
+}
